1f3d2223b231e56035e8785bb6eec5f865718f44
alafiab <alafia.beckford@hotmail.com>
2018-02-24 13:17:15 -0500

    exercise one

--- app/views.py
+++ app/views.py
@@ -8,6 +8,7 @@ import os
 from app import app
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from werkzeug.utils import secure_filename
+from forms import UploadForm
 
 
 ###
@@ -23,24 +24,35 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="File Uploads")
 
 
 @app.route('/upload', methods=['POST', 'GET'])
 def upload():
     if not session.get('logged_in'):
         abort(401)
+    uploadform = UploadForm()
 
     # Instantiate your form class
-
+ 
     # Validate file upload on submit
     if request.method == 'POST':
+        if uploadform.validate_on_submit():
+            print uploadform.csrf_token
         # Get file data and save to your uploads folder
+            photo = uploadform.photo.data
+            description = uploadform.description.data 
 
-        flash('File Saved', 'success')
-        return redirect(url_for('home'))
+            filename = secure_filename(photo.filename)
+            photo.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+        
 
-    return render_template('upload.html')
+            flash('File Saved', 'success')
+            return redirect(url_for('home',filename = filename, description= description))
+        else:
+            print uploadform.errors.items()
+            flash('File not saved','error')
+    return render_template('upload.html',form= uploadform)
 
 
 @app.route('/login', methods=['POST', 'GET'])
